7c540a04862c756529346dfd534285489efe8a30
Faiza Siddiqui <faiza.siddiqui@city.ac.uk>
2023-03-20 08:18:27 +0000

    Setup Application structure pushed to git

---
+++ README.md
@@ -0,0 +1,2 @@
+# GSK-IP-Workday-System
+A system to keep the timesheet tracking, effort tracking and holidays of students/employees managed in one place, handled by the respective manager.
---
+++ gsk-frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coming soon!</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          GSK-IP-Workday System application is coming soon stay tuned!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
